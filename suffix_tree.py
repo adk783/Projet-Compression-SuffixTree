a9429c01737e3b21a14010121cd6676b9783b346
adk783 <antoine.dupuy01@gmail.com>
2026-01-12 13:45:57 +0100

    Add suffix_tree.py for Suffix Tree implementation
    This module implements a Suffix Tree structure using a naive construction algorithm.

---
+++ suffix_tree.py
@@ -0,0 +1,9 @@
+"""
+SUFFIX TREE MODULE
+------------------
+This file contains the implementation of the Suffix Tree structure.
+It allows building a tree from a text using a naive construction algorithm (O(n^2)).
+Used as the base structure for the Ziv-Lempel compression.
+
+Authors: [Dev1]
+"""
